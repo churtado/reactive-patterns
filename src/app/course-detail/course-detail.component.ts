4e45955c8c92b43eed26576c84c37962b8acc974
churtado <charliepapa2@gmail.com>
2019-07-07 22:41:28 +0100

    initial commit

---
+++ src/app/course-detail/course-detail.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from "../shared/model/course";
+import { Lesson } from "../shared/model/lesson";
+import * as _ from 'lodash';
+import { CoursesService } from 'app/services/courses.service';
+
+
+@Component({
+    selector: 'course-detail',
+    templateUrl: './course-detail.component.html',
+    styleUrls: ['./course-detail.component.css']
+})
+export class CourseDetailComponent implements OnInit {
+
+    course: Course;
+    lessons: Lesson[];
+
+    constructor(private route: ActivatedRoute, private coursesService: CoursesService) {
+
+
+        route.params
+            .subscribe(params => {
+
+                const courseUrl = params['id'];
+
+                this.coursesService.findCourseByUrl(courseUrl)
+                    .subscribe(data => {
+                        this.course = data;
+
+                        this.coursesService.findLessonsForCourse(this.course.id)
+                            .subscribe(lessons => this.lessons = lessons);
+                    });
+
+            });
+
+    }
+
+    ngOnInit() {
+
+    }
+
+}
